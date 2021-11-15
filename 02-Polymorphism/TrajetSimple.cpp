30f8533694e372b339857c191c7d4470e4c9d9a2
1gni5 <1gni5@net-c.com>
2021-11-15 23:49:29 +0100

    Create skeleton for TrajetSimple class

---
+++ 02-Polymorphism/TrajetSimple.cpp
@@ -0,0 +1,51 @@
+/*************************************************************************
+                           TrajetSimple  -  description
+                             -------------------
+    début                : 15/11/2021
+    copyright            : (C) 2021 par Jules DUCANGE et Saad GUESSOUS
+    e-mail               : Jules.Ducange@insa-lyon.fr
+                           Saad.Guessous@insa-lyon.fr
+*************************************************************************/
+
+//---------- Réalisation de la classe <TrajetSimple> (fichier TrajetSimple.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "TrajetSimple.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type TrajetSimple::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+//-------------------------------------------- Constructeurs - destructeur
+
+TrajetSimple::TrajetSimple ( const char* VilleDepart, const char* VilleArrivee, const char* MoyenTransport)
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de TrajetSimple
+
+
+TrajetSimple::~TrajetSimple ( )
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <TrajetSimple>" << endl;
+#endif
+} //----- Fin de ~TrajetSimple
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
