2b984786aeaf8294e37a9940aae2cf835903f9f2
Saad <saad.guessous@insa-lyon.fr>
2021-11-15 16:43:46 +0100

    Add TrajetCompose and TrajetSimple classes

---
+++ 02-Polymorphism/TrajetCompose.h
@@ -0,0 +1,62 @@
+/*************************************************************************
+                           TrajetCompose  -  description
+                             -------------------
+    début                : 15/11/2021
+    copyright            : (C) 2021 par Jules DUCANGE et Saad GUESSOUS
+    e-mail               : Jules.Ducange@insa-lyon.fr 
+                           Saad.Guessous@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface de la classe <TrajetCompose> (fichier TrajetCompose.h) ----------------
+#if ! defined ( TrajetCompose_H )
+#define TrajetCompose_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Trajet.h"
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <TrajetCompose>
+//
+//
+//------------------------------------------------------------------------
+
+class TrajetCompose : public Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+//-------------------------------------------- Constructeurs - destructeur
+    TrajetCompose ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~TrajetCompose ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <TrajetCompose>
+
+#endif // TrajetCompose_H
+
---
+++ 02-Polymorphism/TrajetSimple.h
@@ -0,0 +1,63 @@
+/*************************************************************************
+                           TrajetSimple  -  description
+                             -------------------
+    début                : 15/11/2021
+    copyright            : (C) 2021 par Jules DUCANGE et Saad GUESSOUS
+    e-mail               : Jules.Ducange@insa-lyon.fr 
+                           Saad.Guessous@insa-lyon.fr
+*************************************************************************/
+
+//---------- Interface de la classe <TrajetSimple> (fichier TrajetSimple.h) ----------------
+#if ! defined ( TrajetSimple_H )
+#define TrajetSimple_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Trajet.h"
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <TrajetSimple>
+//
+//
+//------------------------------------------------------------------------
+
+class TrajetSimple : public Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//-------------------------------------------- Constructeurs - destructeur
+    TrajetSimple ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~TrajetSimple ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <TrajetSimple>
+
+#endif // TrajetSimple_H
+
