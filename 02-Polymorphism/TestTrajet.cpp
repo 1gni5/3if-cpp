510ac324ae560faca47ecad33b550bf73af483e6
1gni5 <1gni5@net-c.com>
2021-11-18 14:47:25 +0100

    Add testcases for Cellule

--- 02-Polymorphism/TestTrajet.cpp
+++ 02-Polymorphism/TestTrajet.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 //------------------------------------------------------ Include personnel
 #include "TrajetSimple.h"
+#include "Cellule.h"
 
 ///////////////////////////////////////////////////////////////////  PRIVE
 //------------------------------------------------------------- Constantes
@@ -59,13 +60,62 @@ static void TestTrajetSimpleAfficher( void )
   cout << "Trajet "; trajetSimple.Afficher();
 } //----- fin de TestTrajetSimpleGetVilleDepart
 
+static void TestCelluleConstructeur( void )
+// Mode d'emploi : Test le constructeur de TrajetSimple.
+//
+{
+  TrajetSimple trajetSimple = TrajetSimple("Paris", "Lyon", "TGV");
+  TrajetSimple *pointeurTrajetSimple = &trajetSimple;
+  Cellule cellule = Cellule( pointeurTrajetSimple, NULL );
+} //----- fin de TestTrajetSimpleGetVilleDepart
+
+
+static void TestCelluleGetValeur( void )
+// Mode d'emploi : Test le constructeur de TrajetSimple.
+//
+{
+  TrajetSimple trajetSimple = TrajetSimple("Paris", "Lyon", "TGV");
+  TrajetSimple *pointeurTrajetSimple = &trajetSimple;
+  Cellule cellule = Cellule( pointeurTrajetSimple, NULL );
+  cout << "Valeur de la cellule " << cellule.GetValeur() << endl;
+} //----- fin de TestTrajetSimpleGetVilleDepart
+
+static void TestCelluleGetSuivante( void )
+// Mode d'emploi : Test le constructeur de TrajetSimple.
+//
+{
+  TrajetSimple trajetSimple = TrajetSimple("Paris", "Lyon", "TGV");
+  TrajetSimple *pointeurTrajetSimple = &trajetSimple;
+  Cellule cellule = Cellule( pointeurTrajetSimple, NULL );
+  cout << "Cellule suivante : " << cellule.GetSuivante() << endl;
+} //----- fin de TestTrajetSimpleGetVilleDepart
+
+static void TestCelluleAfficher( void )
+// Mode d'emploi : Test le constructeur de TrajetSimple.
+//
+{
+  TrajetSimple trajetSimple = TrajetSimple("Paris", "Lyon", "TGV");
+  TrajetSimple *pointeurTrajetSimple = &trajetSimple;
+  Cellule cellule = Cellule( pointeurTrajetSimple, NULL );
+  cellule.Afficher( );
+} //----- fin de TestTrajetSimpleGetVilleDepart
+
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
 int main ( )
 {
+  /* Test de TrajetSimple */
+  cout << " ----- Test de TrajetSimple ----- " << endl;
   TestTrajetSimpleConstructor();
   TestTrajetSimpleGetVilleDepart();
   TestTrajetSimpleGetVilleArrivee();
   TestTrajetSimpleAfficher();
+
+  /* Test de Cellule */
+  cout << " ----- Test de Cellule ----- " << endl;
+  TestCelluleConstructeur();
+  TestCelluleGetValeur();
+  TestCelluleGetSuivante();
+  TestCelluleAfficher();
   return 0;
 }
